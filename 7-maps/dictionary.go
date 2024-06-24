22f1f5f7c70243aea9b67baa451aa233f0d2ece4
ccsimmi <ffsimmi@gmail.com>
2024-06-24 22:26:26 +0100

    TDD with chapter 7 PART two: maps

--- 7-maps/dictionary.go
+++ 7-maps/dictionary.go
@@ -1,10 +1,13 @@
 package main
 
-import "errors"
+const (
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update non-existing word")
+)
 
 type Dictionary map[string]string
-
-var ErrNotFound = errors.New("could not find the word you were looking for")
+type DictionaryErr string
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -15,6 +18,40 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
